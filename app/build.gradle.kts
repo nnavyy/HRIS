3603c56902ae619583b97e0d5510ad0c3df5ce60
Akung <akung@example.com>
2026-07-03 07:41:46 +0700

    chore: bump version to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         minSdk = 24
         targetSdk = 36
         multiDexEnabled = true
-        versionCode = 3
-        versionName = "1.1.11"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
